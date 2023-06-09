9bedb68bf331995999d5958a7e16aeb7000df734
Daniel Quintero <daniel0725q@gmail.com>
2023-06-08 21:48:50 -0500

    Second commit

--- src/components/PanelControl.jsx
+++ src/components/PanelControl.jsx
@@ -3,38 +3,39 @@ import axios from 'axios';
 import Metricas from '../Metricas';
 
 const PanelControl = () => {
-  const [TotalProductos, setTotalProductos] = useState(0);
-  const [TotalPedidos, setTotalPedidos] = useState(0);
-  const [IngresosTotales, setIngresosTotales] = useState(0);
-  const [PrecioPromedioProductos, setPrecioPromedioProductos] = useState(0);
-  const [ProductosMasVendidos, setProductosMasVendidos] = useState([]);
+  const [totalProductos, setTotalProductos] = useState(0);
+  const [totalPedidos, setTotalPedidos] = useState(0);
+  const [ingresosTotales, setIngresosTotales] = useState(0);
+  const [precioPromedioProductos, setPrecioPromedioProductos] = useState(0);
+  const [productosMasVendidos, setProductosMasVendidos] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchData = () => {
+      var productsResponse, ordersResponse;
       try {
-        const productsResponse = await axios.get('https://fakestoreapi.com/products');
-        const ordersResponse = await axios.get('https://fakestoreapi.com/carts');
-
-        setTotalProductos(productsResponse.data.length);
-        setTotalPedidos(ordersResponse.data.length);
-
-        const ingresos = ordersResponse.data.reduce((total, order) => total + order.total, 0);
-        setIngresosTotales(ingresos);
-
-        const precios = productsResponse.data.map((product) => product.precio);
-        const promedio = precios.reduce((total, precio) => total + precio, 0) / precios.length;
-        setPrecioPromedioProductos(promedio);
-
-        const masVendidos = productsResponse.data.sort((a, b) => b.rating.count - a.rating.count).slice(0, 5);
-        setProductosMasVendidos(masVendidos);
-
-        setLoading(false);
+        axios.get('https://fakestoreapi.com/products').then(res => {
+          setTotalProductos(res.data.length);
+          productsResponse = res.data;
+          let precios = productsResponse.map((product) => product.price);
+          let promedio = precios.reduce((total, precio) => total + precio, 0) / productsResponse.length;
+          setPrecioPromedioProductos(promedio);
+          let masVendidos = productsResponse.sort((a, b) => b.rating.count - a.rating.count).slice(0, 5);
+          setProductosMasVendidos(masVendidos);
+        });
+        axios.get('https://fakestoreapi.com/carts').then(res => {
+          setTotalPedidos(res.data.length);
+          ordersResponse = res.data;
+          console.log(ordersResponse);
+          let ingresos = ordersResponse.reduce((total, order) => total + order.total, 0);
+          setIngresosTotales(ingresos);
+        });
       } catch (error) {
         setError('Error fetching data');
         setLoading(false);
       }
+      setLoading(false);
     };
 
     fetchData();
@@ -48,11 +49,11 @@ const PanelControl = () => {
       {error && <p>{error}</p>}
       {!loading && !error && (
         <>
-          <Metricas label="Número total de productos" value={TotalProductos} />
-          <Metricas label="Número total de pedidos" value={TotalPedidos} />
-          <Metricas label="Ingresos totales generados" value={IngresosTotales} />
-          <Metricas label="Precio promedio de los productos" value={PrecioPromedioProductos} />
-          <Metricas label="Productos más vendidos" data={ProductosMasVendidos} />
+          <Metricas label="Número total de productos" value={totalProductos} />
+          <Metricas label="Número total de pedidos" value={totalPedidos} />
+          <Metricas label="Ingresos totales generados" value={ingresosTotales} />
+          <Metricas label="Precio promedio de los productos" value={precioPromedioProductos} />
+          <Metricas label="Productos más vendidos" data={productosMasVendidos} />
         </>
       )}
     </div>
